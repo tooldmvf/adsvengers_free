e4cc01a3782bfda4b6538007b733cee978ede848
Stefano Caleffi <tool@dmvf.net>
2023-06-06 17:12:09 +0000

    fix

---
+++ pages/NetworkOffers/jsobjects/Utils2/Utils2.js
@@ -0,0 +1,16 @@
+export default {
+	myCurrency: (input) => {
+		const num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+    return formatter.format(num); 
+	},
+	
+	onLoadPage: async () => {
+		await get_all_users.run();
+		await SelectOFFERSUser.run();
+	},
+	
+}
