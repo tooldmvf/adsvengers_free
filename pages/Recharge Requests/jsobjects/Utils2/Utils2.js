dc4f1c8747372d09478b9875273e0a92c82c3bcc
Stefano Caleffi <tool@dmvf.net>
2023-06-18 09:41:01 +0000

    fix

--- pages/Recharge Requests/jsobjects/Utils2/Utils2.js
+++ pages/Recharge Requests/jsobjects/Utils2/Utils2.js
@@ -7,4 +7,26 @@ export default {
 		});
     return formatter.format(num); 
 	},
+	
+	myCurrency2: (input,curr) => {
+		var num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: curr,
+		});
+    return formatter.format(num); 
+	},
+	
+	myCurrency3: async () => {
+		var num = UsersLogTot.data.filter(t => t.recharge_currency == 'USD')[0].recharge_amount
+		const val = await Eur_value.run();
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+		num *= val.eur;
+		num += UsersLogTot.data.filter(t => t.recharge_currency == 'EUR')[0].recharge_amount
+		await storeValue('myTot',formatter.format(num))
+    //return formatter.format(num); 
+	}
 }
