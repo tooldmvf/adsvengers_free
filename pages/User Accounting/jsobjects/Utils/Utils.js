e4b13a56f1366b3e1887ea3627c1a2653815f78c
Stefano Caleffi <tool@dmvf.net>
2023-06-23 07:42:29 +0000

    fix

--- pages/User Accounting/jsobjects/Utils/Utils.js
+++ pages/User Accounting/jsobjects/Utils/Utils.js
@@ -24,6 +24,7 @@ export default {
 	
 	async reload () {
 		await Fetch_Commission_fee.run();
+		await Fetch_Debt_Prev_Month.run();
 		await Fetch_Data_Other_Prj.run();
 		await Fetch_Data_Rev_Ai20.run();
 		await Fetch_Data_Rev_TOT.run();
