60cf3f84d89712423b6d6d1128ada165ba900a6c
Stefano Caleffi <tool@dmvf.net>
2023-06-23 17:26:54 +0000

    fix

--- pages/User Comm Trend Admin/jsobjects/Utils2/Utils2.js
+++ pages/User Comm Trend Admin/jsobjects/Utils2/Utils2.js
@@ -12,9 +12,10 @@ export default {
 		await get_all_users.run();
 		await Fetch_Data_All.run();
 		await Fetch_Data_Graphs.run();
-		await Fetch_Data_Factory.run();
+		await Fetch_Profit_Factory.run();
 		await Fetch_Data_Tot.run();
-		await Fetch_Data_Factory_Tot.run();
+		await Fetch_Profit_Factory_Tot.run();
+		await Fetch_Debt_Factory.run();
 	},
 	
 }
