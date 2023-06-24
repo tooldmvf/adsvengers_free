4749b1d7132c600aa40a5dc09a18f0f38b5690ad
Stefano Caleffi <tool@dmvf.net>
2023-06-24 13:46:14 +0000

    fix

---
+++ pages/Manager Admin/jsobjects/Utils/Utils.js
@@ -0,0 +1,16 @@
+export default {
+	myCurrency: (input) => {
+		const num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+    return formatter.format(num); 
+	},
+	
+	async reload () {
+		await Fetch_Month.run();
+		await Fetch_MB_shares.run();
+		await Fetch_MB_shares_TOT.run();
+	}
+}
