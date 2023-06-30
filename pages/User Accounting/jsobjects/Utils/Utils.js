70555ed3c6acb42ef989d7c8c068c64bcb0586d9
Stefano Caleffi <tool@dmvf.net>
2023-06-30 15:43:19 +0000

    fix

--- pages/User Accounting/jsobjects/Utils/Utils.js
+++ pages/User Accounting/jsobjects/Utils/Utils.js
@@ -23,17 +23,19 @@ export default {
 	},
 	
 	async reload () {
-		await resetWidget('NumberSlider1'); 
-		await resetWidget('Adjust');
-		await Fetch_Adjustments.run();
-		await resetWidget('AdjustProfit');
-		await Fetch_Commission_fee.run();
-		await Fetch_Debt_Prev_Month.run();
-		await Fetch_Data_Rev_TOT.run();
-		await Fetch_Data_Spent_TOT.run();
-		await Fetch_Data_Rev.run();
-		await Fetch_Data_Spent.run();
-		await Fetch_Data_Other_Prj.run();
-		await Fetch_Data_Rev_Ai20.run();
+		if(SelectMonth.selectedOptionValue.length > 0) {
+			await resetWidget('NumberSlider1'); 
+			await resetWidget('Adjust');
+			await Fetch_Adjustments.run();
+			await resetWidget('AdjustProfit');
+			await Fetch_Commission_fee.run();
+			await Fetch_Debt_Prev_Month.run();
+			await Fetch_Data_Rev_TOT.run();
+			await Fetch_Data_Spent_TOT.run();
+			await Fetch_Data_Rev.run();
+			await Fetch_Data_Spent.run();
+			await Fetch_Data_Other_Prj.run();
+			await Fetch_Data_Rev_Ai20.run();
+		}
 	}
 }
