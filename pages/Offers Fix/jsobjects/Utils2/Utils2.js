47e9452f6c54a2f2111cda5318cef612bbbe5895
tool <tool@dmvfltd.com>
2024-12-01 07:04:41 +0000

    offer Fix page

---
+++ pages/Offers Fix/jsobjects/Utils2/Utils2.js
@@ -0,0 +1,11 @@
+export default {
+	myCurrency: (input) => {
+		const num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+    return formatter.format(num); 
+	},
+	
+}
