780d6cf255ae97fbed326641d9bea705eeded9d3
tool <tool@dmvfltd.com>
2024-12-14 17:28:11 +0000

    Arb spent

---
+++ pages/Arb Spent Check/jsobjects/Utils2/Utils2.js
@@ -0,0 +1,15 @@
+export default {
+	myCurrency: (input) => {
+		const num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+    return formatter.format(num); 
+	},
+	
+	onLoad: async () => {
+		await get_all_users.run();
+		await get_all_bm.run();
+	}
+}
