b58527bfb2999edfa934a250b1af4c6697b213fc
Stefano Caleffi <tool@dmvf.net>
2025-01-23 06:44:41 +0000

    Added monthly network analysis

---
+++ pages/User Comm Fra/jsobjects/Utils2/Utils2.js
@@ -0,0 +1,40 @@
+export default {
+	myCurrency: (input) => {
+		const num = input;
+		const formatter = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency: 'EUR',
+		});
+    return formatter.format(num); 
+	},
+	
+	onLoadPage: async () => {
+		await get_all_users.run();
+		await Fetch_Data_All.run();
+		await Fetch_Data_Graphs.run();
+		await Fetch_Data_Tot.run();
+		await Fetch_Debt_Factory.run();
+		await Fetch_Invoices_Factory.run();
+		await Fetch_Mail_Sent.run();
+		await Fetch_Profit_Factory.run();
+		await Fetch_Profit_Factory_Tot.run();
+		await Fetch_Revenue_Factory.run();
+		await Fetch_Roi_Factory.run();
+		await Fetch_Spent_Factory.run();
+	},
+	
+	onLoadPage2: async () => {
+		await Fetch_Data_All.run();
+		await Fetch_Data_Graphs.run();
+		await Fetch_Data_Tot.run();
+		await Fetch_Debt_Factory.run();
+		await Fetch_Invoices_Factory.run();
+		await Fetch_Mail_Sent.run();
+		await Fetch_Profit_Factory.run();
+		await Fetch_Profit_Factory_Tot.run();
+		await Fetch_Revenue_Factory.run();
+		await Fetch_Roi_Factory.run();
+		await Fetch_Spent_Factory.run();
+	},
+	
+}
